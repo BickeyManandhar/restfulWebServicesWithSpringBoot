ae8354b728d75e28aa7b90127cb6ea675df18bee
BickeyManandhar <manandharbickey@gmail.com>
2024-06-15 19:11:21 -0400

    initial commit

---
+++ src/main/java/com/restful_webservice_with_springboot/rest_webservice_with_spring_boot/RestWebserviceWithSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.restful_webservice_with_springboot.rest_webservice_with_spring_boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestWebserviceWithSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestWebserviceWithSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/restful_webservice_with_springboot/rest_webservice_with_spring_boot/RestWebserviceWithSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.restful_webservice_with_springboot.rest_webservice_with_spring_boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestWebserviceWithSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
