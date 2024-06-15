8c2385acf9b59b59890610ed56a5fd46d69bde41
BickeyManandhar <manandharbickey@gmail.com>
2024-06-15 19:17:45 -0400

    first hello world api with GetMapping

--- src/main/java/com/restful_webservice_with_springboot/rest_webservice_with_spring_boot/helloworld/HelloWorld.java
+++ src/main/java/com/restful_webservice_with_springboot/rest_webservice_with_spring_boot/helloworld/HelloWorld.java
@@ -1,13 +1,12 @@
 package com.restful_webservice_with_springboot.rest_webservice_with_spring_boot.helloworld;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorld {
 
-    @RequestMapping(method= RequestMethod.GET,path="/hello-world")
+    @GetMapping
     public String getHelloWorld(){
         return "Hello World.";
     }
