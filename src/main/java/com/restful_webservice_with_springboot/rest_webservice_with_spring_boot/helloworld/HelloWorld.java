32e31192f163ff94b55fbcff7dd7f8c1c80aa22f
BickeyManandhar <manandharbickey@gmail.com>
2024-06-15 19:49:49 -0400

    using PathVariable

--- src/main/java/com/restful_webservice_with_springboot/rest_webservice_with_spring_boot/helloworld/HelloWorld.java
+++ src/main/java/com/restful_webservice_with_springboot/rest_webservice_with_spring_boot/helloworld/HelloWorld.java
@@ -1,6 +1,7 @@
 package com.restful_webservice_with_springboot.rest_webservice_with_spring_boot.helloworld;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -14,5 +15,13 @@ public class HelloWorld {
     @GetMapping(path="/hello-world-bean")
     public HelloWorldBean getHelloWorldBean(){
         return new HelloWorldBean("Hello World."); //returning instance of our own class, return JSON
+    }
+
+    //path parameter
+    @GetMapping(path="/hello-world/path-variable/{name}")
+    public HelloWorldBean getPathParameter(@PathVariable String name){
+        return new HelloWorldBean(
+                String.format("Hello %s", name.toUpperCase())
+        );
     }
 }
