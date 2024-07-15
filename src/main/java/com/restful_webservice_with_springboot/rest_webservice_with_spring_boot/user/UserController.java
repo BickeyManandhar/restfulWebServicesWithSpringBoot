7527f820207c579eb3d4f5a1ca238b1518ce0b94
BickeyManandhar <manandharbickey@gmail.com>
2024-07-15 13:49:58 -0400

    using HATEOAS and implementing HAL using EntityModel and WebMvcLinkBuilder in getUser() to get the link to getAllUsers() in response

--- src/main/java/com/restful_webservice_with_springboot/rest_webservice_with_spring_boot/user/UserController.java
+++ src/main/java/com/restful_webservice_with_springboot/rest_webservice_with_spring_boot/user/UserController.java
@@ -1,12 +1,16 @@
 package com.restful_webservice_with_springboot.rest_webservice_with_spring_boot.user;
 
 import jakarta.validation.Valid;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
 import java.util.List;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @RestController
 public class UserController {
@@ -22,13 +26,21 @@ public class UserController {
         return userDaoService.findAll();
     }
 
+
+    //we want to add a link to http://localhost:8080/users using hateoas
+    //Concept Required:
+    //EntityModel - we want to wrap the User model with EntityModel because we do not want to change anything inside User bean
+    //WebMvcLinkBuilder
     @GetMapping(path = "/users/{user_id}")
-    public User getUser(@PathVariable int user_id){
+    public EntityModel<User> getUser(@PathVariable int user_id){
         User oneUser = userDaoService.findOne(user_id);
         if(oneUser==null) {
             throw new UserNotFoundException("user_id: "+user_id);
         }
-        return oneUser;
+        EntityModel<User> entityModel = EntityModel.of(oneUser);
+        WebMvcLinkBuilder link = WebMvcLinkBuilder.linkTo(methodOn(this.getClass()).getAllUsers()); //getting link of method getAllUsers()
+        entityModel.add(link.withRel("all-users")); //adding the link to entityModel and also specifying the meaningful key value that we will see in response
+        return entityModel;
     }
 
     @PostMapping(path = "/users")
