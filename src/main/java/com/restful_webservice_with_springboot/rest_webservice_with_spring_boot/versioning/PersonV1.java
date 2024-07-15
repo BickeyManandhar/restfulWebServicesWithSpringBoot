3b364f44542952f2a3a31f738c6608683cce173e
BickeyManandhar <manandharbickey@gmail.com>
2024-07-15 13:08:00 -0400

    versioning by URL

---
+++ src/main/java/com/restful_webservice_with_springboot/rest_webservice_with_spring_boot/versioning/PersonV1.java
@@ -0,0 +1,20 @@
+package com.restful_webservice_with_springboot.rest_webservice_with_spring_boot.versioning;
+
+public class PersonV1 {
+    private String name;
+
+    public PersonV1(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        return "PersonV1{" +
+                "name='" + name + '\'' +
+                '}';
+    }
+}
---
+++ src/main/java/com/restful_webservice_with_springboot/rest_webservice_with_spring_boot/versioning/PersonV2.java
@@ -0,0 +1,20 @@
+package com.restful_webservice_with_springboot.rest_webservice_with_spring_boot.versioning;
+
+public class PersonV2 {
+    private Name name;
+
+    public PersonV2(Name name) {
+        this.name = name;
+    }
+
+    public Name getName() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        return "PersonV1{" +
+                "name='" + name + '\'' +
+                '}';
+    }
+}
