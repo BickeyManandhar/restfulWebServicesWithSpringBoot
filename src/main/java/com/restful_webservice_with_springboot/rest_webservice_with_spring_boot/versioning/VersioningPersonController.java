02c7bb97ef30c497a18c00ffc23102287557e9e7
BickeyManandhar <manandharbickey@gmail.com>
2024-07-15 13:16:17 -0400

    versioning by request parameters

--- src/main/java/com/restful_webservice_with_springboot/rest_webservice_with_spring_boot/versioning/VersioningPersonController.java
+++ src/main/java/com/restful_webservice_with_springboot/rest_webservice_with_spring_boot/versioning/VersioningPersonController.java
@@ -1,18 +1,33 @@
 package com.restful_webservice_with_springboot.rest_webservice_with_spring_boot.versioning;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class VersioningPersonController {
 
+    //versioning by URI
     @GetMapping("/v1/person")
     public PersonV1 getPersonV1(){
         return new PersonV1("John Cena");
     }
 
+    //versioning by URI
     @GetMapping("/v2/person")
     public PersonV2 getPersonV2(){
+        return new PersonV2(new Name("John","Cena"));
+    }
+
+    //versioning by parameter
+    @GetMapping(path="/person", params="version=1")
+    public PersonV1 getPersonV1ByRequestParameter(){
+        return new PersonV1("John Cena");
+    }
+
+    //versioning by parameter
+    @GetMapping(path="/person", params="version=2")
+    public PersonV2 getPersonV2ByRequestParameter(){
         return new PersonV2(new Name("John","Cena"));
     }
 }
