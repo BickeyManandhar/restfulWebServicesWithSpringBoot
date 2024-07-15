ba752be74c8ed74774f042de9e8ab2eee5a13e2e
BickeyManandhar <manandharbickey@gmail.com>
2024-07-15 13:06:20 -0400

    added exception

--- src/main/java/com/restful_webservice_with_springboot/rest_webservice_with_spring_boot/exception/CustomizedResponseEntityExceptionHandler.java
+++ src/main/java/com/restful_webservice_with_springboot/rest_webservice_with_spring_boot/exception/CustomizedResponseEntityExceptionHandler.java
@@ -1,8 +1,11 @@
 package com.restful_webservice_with_springboot.rest_webservice_with_spring_boot.exception;
 
 import com.restful_webservice_with_springboot.rest_webservice_with_spring_boot.user.UserNotFoundException;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
@@ -23,5 +26,11 @@ public class CustomizedResponseEntityExceptionHandler extends ResponseEntityExce
     public final ResponseEntity<ErrorDetails> handleUserNotFoundException(Exception ex, WebRequest request){
         ErrorDetails errorDetails = new ErrorDetails(LocalDateTime.now(), ex.getMessage(),request.getDescription(false));
         return new ResponseEntity<ErrorDetails>(errorDetails, HttpStatus.NOT_FOUND);
+    }
+    //why override here? we first checked what error it is giving. then went to ResponseEntityExceptionHandler class and copied that method and made changes to make it work for our case
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+        ErrorDetails errorDetails = new ErrorDetails(LocalDateTime.now(), ex.getFieldError().getDefaultMessage(),request.getDescription(false));
+        return new ResponseEntity<>(errorDetails, HttpStatus.BAD_REQUEST);
     }
 }
